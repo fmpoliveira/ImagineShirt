c6b9d49b74dcfd5bb402e34e87b23c829bd7d27b
kardeira <96345907+kardeira@users.noreply.github.com>
2023-06-21 23:55:36 +0100

    format

--- resources/views/emails/newOrder.blade.php
+++ resources/views/emails/newOrder.blade.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Processing Notification</title>
 </head>
+
 <body>
-    <table style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; border-collapse: collapse; border: 1px solid #ccc;">
+    <table
+        style="width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; border-collapse: collapse; border: 1px solid #ccc;">
         <tr>
             <td style="text-align: center;">
                 <h2>Order Processing Notification</h2>
@@ -15,7 +18,8 @@
         <tr>
             <td>
                 <p>Dear {{ $user->name }},</p>
-                <p>We wanted to inform you that your order number {{ $order->id }} is currently being processed. We appreciate your business and will keep you updated on the progress.</p>
+                <p>We wanted to inform you that your order number {{ $order->id }} is currently being processed. We
+                    appreciate your business and will keep you updated on the progress.</p>
                 <p>If you have any questions or concerns, please don't hesitate to contact our support team.</p>
                 <p>Thank you for choosing our services!</p>
             </td>
@@ -27,4 +31,5 @@
         </tr>
     </table>
 </body>
+
 </html>
